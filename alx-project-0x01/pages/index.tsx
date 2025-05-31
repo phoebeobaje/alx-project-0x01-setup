9e9d77182ec55bde84e81c97f356eabaa1e20390
phoebeobaje <phoeberobertscareer@gmail.com>
2025-05-31 22:55:32 +0100

    added

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
@@ -6,18 +7,19 @@ const Home: React.FC = () => {
       <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
         <div className="text-center">
           <h1 className="text-5xl font-bold text-white">
-            Welcome to our Application
+            Welcome to our Application!
           </h1>
           <p className="mt-4 text-xl text-white">
-            We are glad you are here. Explore and enjoy your experience.
+            We're glad you're here. Explore and enjoy your experience.
           </p>
           <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 transition">
             Get Started
           </button>
-        </div>
+            </div>
       </main>
     </div>
-  );
-};
+  )
+}
 
 export default Home;
+// this has been replaced please
