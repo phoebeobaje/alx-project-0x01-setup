b34285d83df2a3fabfd81572aded35ece1cb8e80
phoebeobaje <phoeberobertscareer@gmail.com>
2025-06-01 15:52:03 +0100

    usermodal

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,24 +2,39 @@ import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-
-const Users: React.FC<UserProps[]> = ({posts}) => {  
-    return(
+import { useState } from "react";
+import { UserData } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+const Users: React.FC<UserProps[]> = ({users}) => {  
+  const [isModalOpen, setModalOpen] = useState(false);
+    const [user, setUser] = useState<UserData | null>(null);
+  
+  
+    const handleAddUser = (newUser: UserData) => {
+      setUser({ ...newUser, id: users.length + 1 });
+    };
+   
+  return(
         <div>
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
         <h1 className=" text-2xl font-semibold">Users</h1>
-        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+        <button onClick={()=> setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          Add User</button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
           {
-            posts.map(({ name, email,  username, id, address, phone, website, company}: UserProps, key: number) => (
+            users.map(({ name, email,  username, id, address, phone, website, company}: UserProps, key: number) => (
               <UserCard name={name} email={email} username={username} id={id} address={address} phone={phone} website={website} company={company} key={key} />
             ))
           }
         </div>
-      </main>       
+      </main>    
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
+
        </div>
     )
 }
